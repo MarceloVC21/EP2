7a55c61780edf87fc4078e7d934cb558e11b9bfe
MarceloVC21 <vaccaro2s.marcelo@gmail.com>
2022-05-23 13:22:13 -0700

    EP2

---
+++ Shared/EP2App.swift
@@ -0,0 +1,17 @@
+//
+//  EP2App.swift
+//  Shared
+//
+//  Created by user206623 on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct EP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
